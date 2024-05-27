0b0277e8a59b7dcd3bbc91a0f8597e428f40a62b
MARTIN VIVANCO <martinvivanco@live.com.mx>
2024-05-27 13:43:11 -0600

    Fix image path in AboutMe component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            <p>© 2024 Mi Nombre</p>
+            <p>© 2024 JOSE MARTIN VIVANCO FLORES</p>
             <ul>
                 <li><a href="https://github.com/MartinVF12/react-portfolio">GitHub</a></li>
                 <li><a href="https://www.linkedin.com/feed/">LinkedIn</a></li>
--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -1,17 +1,12 @@
 import React from 'react';
+import profileImage from '../../assets/img.jpeg'; // Ruta correcta a la imagen
 
 function AboutMe() {
     return (
         <section>
             <h2>About Me</h2>
-            <img src="../assets/img.jpeg" alt="Mi Foto" />
-            <p>Hello! I'm currently a Manager of Innovation and Transformation at Banorte, where I focus on developing new and exciting products. I graduated from Tec, where I honed my skills and passion for technology and innovation. Outside of work, I have a deep love for football and Padel, both of which keep me active and energized. I'm also enrolled in a development bootcamp to enhance my skills and explore new career opportunities. I'm eager to leverage my experience and education to make a significant impact in my next role.
-
-
-
-
-
-            </p>
+            <img src={profileImage} alt="Mi Foto" />
+            <p>Hello! I'm currently a Manager of Innovation and Transformation at Banorte, where I focus on developing new and exciting products. I graduated from Tec, where I honed my skills and passion for technology and innovation. Outside of work, I have a deep love for football and Padel, both of which keep me active and energized. I'm also enrolled in a development bootcamp to enhance my skills and explore new career opportunities. I'm eager to leverage my experience and education to make a significant impact in my next role.</p>
         </section>
     );
 }
